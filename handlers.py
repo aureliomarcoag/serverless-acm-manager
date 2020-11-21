c9194a2278403ae8f70c8a076f6e129676ad2afe
Marco Godinho <aurelio.marco.ag@gmail.com>
2020-11-21 15:37:29 +0100

    Added initial unit tests for handlers

--- handlers.py
+++ handlers.py
@@ -1,11 +1,67 @@
 import os
+import re
 import uuid
-from typing import List, Generator
+from typing import List, Generator, Tuple, Dict
 import boto3  # type: ignore
-import certifier
+from certifier import certifier
 
 actions = certifier.actions()
 s3_client = boto3.resource("s3")
+
+
+def get_certificates_from_s3_event(
+    event: Dict,
+) -> Tuple[List[Tuple[str, str, str]], List[Tuple[str, str, str]], List[Tuple[str, str, str]]]:
+    """
+    Returns a tuple containing three lists. The first one is a list of certificates to delete,
+    the second is a list of certificates to create, and the third one contains certificates that failed a validation.
+    Each create and delete list item is a tuple formed by the s3 bucket name, the key of the object in the bucket
+    and the key stripped of file extensions (up to the first dot).
+    The third element of tuples in the list of failed items is the reason for the failure instead of the object key stripped of extensions.
+    The following validation is performed:
+    * Make sure the S3 key only contains letters, numbers and the characters .-_ to make sure it's compatible with Parameter Store.
+    Example of a create list:
+    [("my_bucket", "key/to.my/object.first.txt", "key/to/object")]
+    Example of a failure list:
+    [("my_bucket", "key/to.my/object.first.txt", "the S3 object key contains invalid characters")]
+    """
+    if "Records" not in event:
+        raise KeyError("'Records' key not found in event object")
+
+    pattern = re.compile("([A-Za-z0-9]|-|_|\.|/)+")
+    delete_certificates: List[Tuple[str, str, str]] = []
+    create_certificates: List[Tuple[str, str, str]] = []
+    failed_certificates: List[Tuple[str, str, str]] = []
+
+    for record in event["Records"]:
+        s3_data: Dict = record["s3"]
+        certificate_file_data: Tuple[str, str] = (
+            s3_data["bucket"]["name"],
+            s3_data["object"]["key"],
+        )
+
+        failed_reason = ""
+        if certificate_file_data[1][-1] == "/":
+            failed_reason += "Ignoring S3 folder object: 's3://{}/{}'. ".format(*certificate_file_data)
+
+        if not pattern.fullmatch(certificate_file_data[1]):
+            failed_reason += "The S3 object key '{}' does not match the pattern '{}'. ".format(
+                certificate_file_data[1], pattern.pattern
+            )
+
+        if failed_reason:
+            failed_certificates.append(certificate_file_data + (failed_reason,))
+            continue
+
+        split_key = certificate_file_data[1].split("/")
+        key_stripped_extension = "/".join(split_key[:-1] + [split_key[-1].split(".")[0]])
+
+        if record["eventName"].startswith("ObjectCreated"):
+            create_certificates.append(certificate_file_data + (key_stripped_extension,))
+        if record["eventName"].startswith("ObjectRemoved"):
+            delete_certificates.append(certificate_file_data + (key_stripped_extension,))
+
+    return delete_certificates, create_certificates, failed_certificates
 
 
 def get_file_from_s3(bucket: str, key: str) -> Generator[str, None, None]:
---
+++ tests/test_handlers.py
@@ -0,0 +1,18 @@
+import json
+import pytest
+import handlers
+
+
+def test_get_certificates_from_s3_event(acm_client):
+    with pytest.test_files["s3_event_created.json"].open() as event_created_file, pytest.test_files[
+        "s3_event_removed.json"
+    ].open() as event_removed_file, pytest.test_files["s3_event_invalid_item.json"].open() as event_invalid_item_file:
+        event_created = json.loads(event_created_file.read())
+        event_removed = json.loads(event_removed_file.read())
+        event_invalid_item = json.loads(event_invalid_item_file.read())
+    delete, create, failed = handlers.get_certificates_from_s3_event(event_created)
+    assert len(create) == 1 and len(delete) == len(failed) == 0
+    delete, create, failed = handlers.get_certificates_from_s3_event(event_removed)
+    assert len(delete) == 1 and len(create) == len(failed) == 0
+    delete, create, failed = handlers.get_certificates_from_s3_event(event_invalid_item)
+    assert len(failed) == 1 and len(create) == len(delete) == 0
