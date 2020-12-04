2af2f46833701c2dbaaad9cdfe7222a9f1cafaf6
Marco Godinho <aurelio.marco.ag@gmail.com>
2020-12-04 14:42:59 +0100

    Fix call to delete_parameter missing keyword argument

--- certifier/certifier.py
+++ certifier/certifier.py
@@ -150,7 +150,7 @@ class actions:
         ssm_parameter_name = f"/certifier/{certificate.identifier}"
         ssm_parameter = self.ssm_client.get_parameter(Name=ssm_parameter_name)
         if "Parameter" in ssm_parameter and certificate.arn == ssm_parameter["Parameter"]["Value"]:
-            self.ssm_client.delete_parameter(ssm_parameter_name)
+            self.ssm_client.delete_parameter(Name=ssm_parameter_name)
 
     def delete(self, certificates: List[Certificate]) -> Tuple[List[Dict[str, str]], List[Dict[str, str]]]:
         """
